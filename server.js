f26588aabe249a72f9d522c232851bbbe6ce2af2
Rehan-Multani <mrmmultani@gmail.com>
2024-03-04 12:23:36 +0530

    wdwd
    wdw

--- server.js
+++ server.js
@@ -1,68 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = 8000;
 
 app.use(bodyParser.json());
-app.use(cors())
-const countries = require('./countries.json');
+app.use(cors());
+
 const states = require('./states.json');
 const cities = require('./cities.json');
 
-// Endpoint to get all countries
-app.get('/', (req, res) => {
-    const countryNames = countries.map(country => country.name);
-    res.json(countryNames); // Sending an array of country names as a JSON response
-});
-
-
-// Endpoint to get states of a specific country
-app.get('/state', (req, res) => {
-    const state = req.query.state;
-    const countryStates = states.filter(state => state.country_name === state);
-    const countryNames = countryStates.map(country => country.name);
-    res.json(countryNames);
+// Endpoint to get all states
+app.get('/states', (req, res) => {
+    const stateNames = states.map(state => state.name);
+    res.json(stateNames);
 });
 
 // Endpoint to get cities of a specific state
-app.get('/city', (req, res) => {
-    const city = req.query.city;
-    console.log(req.query.city)
-    const countryStates = cities.filter(state => state.state_name == city);
-
-    // const stateCities = cities.filter(city => city.state_code === stateCode);
-    res.json(countryStates);
-});
-app.get('/api', (req, res) => {
-    const country = req.query.country;
-    const city = req.query.state;
-
-    if (country && city) {
-
-        const countryStates = states.filter(state => state.country_name === country);
-
-        const stateCities = cities.filter(state => state.state_name === city);
-
-        if (stateCities) {
-
-            res.json(stateCities);
-        } else {
-
-            res.json([]);
-        }
-    } else if (country) {
-
-        const countryStates = states.filter(state => state.country_name === country);
-        // const stateNames = countryStates.map(state => state.name);
-        res.json(countryStates);
-    } else {
-
-        // const countryNames = countries.map(country => country.name);
-        res.json(countries);
-    }
+app.get('/states/cities', (req, res) => {
+    const stateName = req.query.state;
+    const stateCities = cities.filter(city => city.state_name === stateName);
+    const data = stateCities.map((item) => {
+        return {
+            city: item.name
+        };
+    });
+    
+    res.json(data);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
